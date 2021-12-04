cf3bd09442201c79f55ca8214a1c615186159dd2
sucharita05 <swatisucharita05@gmail.com>
2021-12-05 01:40:11 +0530

    Modified ImageCompressor

--- src/components/ImageCompressor.js
+++ src/components/ImageCompressor.js
@@ -68,75 +68,74 @@ export default class ImageCompressor extends Component {
             <h4>Upload Image</h4>
             <h4>Click on Compressor</h4>
             <h4>Dowload Compressed Image</h4>
-            
           </div>
 
           <div className="row mt-5">
             <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12">
-            <Card style={{ width: '22rem' }}>
-              {this.state.uploadImage ? (
-                <Card.Img
-                  className="ht"
-                  variant="top"
-                  src={this.state.originalLink}
-                ></Card.Img>
-              ) : (
-                <Card.Body>
-                  <Card.Text>
-                    <FontAwesomeIcon icon={faCloudArrowUp} size="10x" />
-                  </Card.Text>
-                  <input
-                    type="file"
-                    accept="image/*"
-                    className="mt-2 btn btn-dark w-75"
-                    onChange={e => this.handleImage(e)}
-                  />
-                </Card.Body>
-              )}
+              <Card style={{ width: '22rem' }}>
+                {this.state.uploadImage ? (
+                  <Card.Img
+                    className="ht"
+                    variant="top"
+                    src={this.state.originalLink}
+                  ></Card.Img>
+                ) : (
+                  <Card.Body>
+                    <Card.Text>
+                      <FontAwesomeIcon icon={faCloudArrowUp} size="10x" />
+                    </Card.Text>
+                    <input
+                      type="file"
+                      accept="image/*"
+                      className="mt-2 btn btn-dark w-75"
+                      onChange={e => this.handleImage(e)}
+                    />
+                  </Card.Body>
+                )}
               </Card>
             </div>
             <div className="col-xl-4 col-lg-4 col-md-12 mb-5 mt-5 col-sm-12 d-flex justify-content-center align-items-baseline">
               <br />
               <Card style={{ width: '10rem' }}>
-              {this.state.outputFileName ? (
-                <button
-                  type="button"
-                  className=" btn btn-dark"
-                  onClick={e => this.handleClick(e)}
-                >
-                  Compress
-                </button>
-              ) : (
-                <></>
-              )}
+                {this.state.outputFileName ? (
+                  <button
+                    type="button"
+                    className=" btn btn-dark"
+                    onClick={e => this.handleClick(e)}
+                  >
+                    Compress
+                  </button>
+                ) : (
+                  <></>
+                )}
               </Card>
             </div>
 
             <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12 mt-3">
-            <Card style={{ width: '20rem' }}>
-              <Card.Body>
-              {this.state.clicked ?
-              (
-                <>
-                <Card.Img variant='top' src={this.state.compressedLink}></Card.Img>
-                <div className="d-flex justify-content-center">
-                  <a
-                    href={this.state.compressedLink}
-                    download={this.state.outputFileName}
-                    className="mt-2 btn btn-dark w-75"
-                  >
-                    Download
-                  </a>
-                </div>
-                </>
-              ) : (
-                <>
-                <Card.Text>
-                    <FontAwesomeIcon icon={faCloudArrowUp} size="10x" />
-                </Card.Text>
-                </>
-              )}
-              </Card.Body>
+              <Card style={{ width: '20rem' }}>
+                <Card.Body>
+                  {this.state.clicked ?
+                    (
+                      <>
+                        <Card.Img variant='top' src={this.state.compressedLink}></Card.Img>
+                        <div className="d-flex justify-content-center">
+                          <a
+                            href={this.state.compressedLink}
+                            download={this.state.outputFileName}
+                            className="mt-2 btn btn-dark w-75"
+                          >
+                            Download
+                          </a>
+                        </div>
+                      </>
+                    ) : (
+                      <>
+                        <Card.Text>
+                          <FontAwesomeIcon icon={faCloudArrowUp} size="10x" />
+                        </Card.Text>
+                      </>
+                    )}
+                </Card.Body>
               </Card>
             </div>
           </div>
